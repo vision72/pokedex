e23e29a6e5b6eef73132fc314a8f339a04db451b
Ali Raza LAKHANI <a.lakhani@planetsmartcity.com>
2024-06-04 07:05:56 +0530

    feat(app): introducing pokedex application

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return <SafeAreaView style={backgroundStyle}></SafeAreaView>;
+}
+
+export default App;
